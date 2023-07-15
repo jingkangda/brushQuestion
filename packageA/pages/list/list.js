d8e75ff92e57c2c92b07b780cbf21dca0fb0b9fd
aub123 <2546319206@qq.com>
2023-07-15 14:42:34 +0800

    feat: delete photo

--- packageA/pages/list/list.js
+++ packageA/pages/list/list.js
@@ -32,6 +32,75 @@ Page({
     });
 
   },
+  deletePhoto: function (e) {
+    var index = e.currentTarget.dataset.index; // 获取当前点击的照片项的索引
+    var imagePath = this.data.imagePaths[index]; // 根据索引获取对应的照片路径
+
+    // 弹出确认删除的模态框
+    wx.showModal({
+      title: '确认删除',
+      content: '确定要删除这张照片吗？',
+      success: (res) => {
+        if (res.confirm) {
+          // 用户点击了确定按钮
+          // 执行删除操作
+          // 调用删除函数或者发送请求到服务器删除照片
+          // 删除成功后更新页面上的照片列表
+          this.deletePhotoAtIndex(index);
+        } else if (res.cancel) {
+          // 用户点击了取消按钮
+          // 可以不做任何操作或者给出提示信息
+        }
+      }
+    });
+  },
+
+  deletePhoto: function (e) {
+    var index = e.currentTarget.dataset.index; // 获取当前点击的照片项的索引
+    var imagePath = this.data.imagePaths[index]; // 根据索引获取对应的照片路径
+
+    // 弹出确认删除的模态框
+    wx.showModal({
+      title: '确认删除',
+      content: '确定要删除这张照片吗？',
+      success: (res) => {
+        if (res.confirm) {
+          // 用户点击了确定按钮
+          // 执行删除操作
+          this.deletePhotoAtIndex(index, imagePath);
+        } else if (res.cancel) {
+          // 用户点击了取消按钮
+          // 可以不做任何操作或者给出提示信息
+        }
+      }
+    });
+  },
+
+  deletePhotoAtIndex: function (index, imagePath) {
+    // 删除文件
+    wx.getFileSystemManager().unlink({
+      filePath: imagePath,
+      success: () => {
+        console.log('文件删除成功');
+        // 文件删除成功后更新页面上的照片列表
+
+        // 获取当前的照片列表
+        var imagePaths = this.data.imagePaths;
+
+        // 从数组中移除被删除的照片路径
+        imagePaths.splice(index, 1);
+
+        // 更新页面上的照片列表
+        this.setData({
+          imagePaths: imagePaths
+        });
+      },
+      fail: (res) => {
+        console.log('文件删除失败', res);
+        // 文件删除失败的处理逻辑，可以给出提示信息等
+      }
+    });
+  },
   clickImg: function (e) {
     console.log(e);
     // var imgUrl = this.data.imgUrl;
@@ -78,12 +147,25 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh() {
-    wx.navigateTo({
-      url: './list.wxml',
-      success: function (res) {},
-      fail: function (res) {},
-      complete: function (res) {},
-    })
+    setTimeout(() => {
+      var that = this;
+      wx.getFileSystemManager().readdir({
+        // dirPath: '/packageA/pages/list/photos/',
+        dirPath: this.data.saveDir,
+        success: function (res) {
+          that.setData({
+            imagePaths: res.files.map(i => `${wx.env.USER_DATA_PATH}/my-directory/` + i)
+          });
+          console.log(res.files);
+        },
+        fail: function (res) {
+          console.log(res.errMsg);
+        }
+      });
+
+    }, 1000);
+
+
   },
 
   /**
